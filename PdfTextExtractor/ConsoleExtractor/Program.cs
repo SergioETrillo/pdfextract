c02d79d12eae370530f8d9a5b577e963bbe9671f
GRANTADESIGN\sergio.trillo <senrech@yahoo.fr>
2018-03-01 17:14:55 +0000

    add console project to play

---
+++ PdfTextExtractor/ConsoleExtractor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PdfTextExtractor;
+
+namespace ConsoleExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PdfTextExtractor.PdfTextExtractor extractor = new global::PdfTextExtractor.PdfTextExtractor();
+            extractor.ExtractTextToFile(@"C:\Users\sergio.trillo\Documents\MayFly18\Najib\test.pdf", @"C:\Users\sergio.trillo\Documents\MayFly18\Najib\test.txt");
+        }
+    }
+}
